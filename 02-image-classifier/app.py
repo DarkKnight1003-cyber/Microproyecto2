7f882915c52a7c6bc6b919a235b0faa8ec67f72b
Juan Esteban Aristizabal <aristizabalgarciaj22@gmail.com>
2026-09-16 13:17:01 -0500

    Reescribe clasificador de imagenes con PyTorch para ARM64, agrega manifiestos de Kubernetes y prueba de HPA

--- 02-image-classifier/app.py
+++ 02-image-classifier/app.py
@@ -1,31 +1,31 @@
-from gluoncv.model_zoo import get_model
-import matplotlib.pyplot as plt
-from mxnet import gluon, nd, image
-from mxnet.gluon.data.vision import transforms
-from gluoncv import utils
-from PIL import Image
 import io
 import flask
+import torch
+from torchvision import models
+from PIL import Image
+
 app = flask.Flask(__name__)
+
+weights = models.ResNet18_Weights.DEFAULT
+net = models.resnet18(weights=weights)
+net.eval()
+
+transform_fn = weights.transforms()
+class_names = weights.meta["categories"]
 
 @app.route("/predict", methods=["POST"])
 def predict():
     if flask.request.method == "POST":
         if flask.request.files.get("img"):
-            img = Image.open(io.BytesIO(flask.request.files["img"].read()))
-            transform_fn = transforms.Compose([
-                transforms.Resize(32),
-                transforms.CenterCrop(32),
-                transforms.ToTensor(),
-                transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
-            img = transform_fn(nd.array(img))
-            net = get_model('cifar_resnet20_v1', classes=10, pretrained=True)
-            pred = net(img.expand_dims(axis=0))
-            class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-                       'dog', 'frog', 'horse', 'ship', 'truck']
-            ind = nd.argmax(pred, axis=1).astype('int')
+            img = Image.open(io.BytesIO(flask.request.files["img"].read())).convert("RGB")
+            img_t = transform_fn(img)
+            batch_t = img_t.unsqueeze(0)
+            with torch.no_grad():
+                pred = net(batch_t)
+            probs = torch.nn.functional.softmax(pred[0], dim=0)
+            ind = torch.argmax(probs).item()
             prediction = ('The input picture is classified as [%s], with probability %.3f.' %
-                         (class_names[ind.asscalar()], nd.softmax(pred)[0][ind].asscalar()))
+                         (class_names[ind], probs[ind].item()))
     return prediction
 
 if __name__ == '__main__':
